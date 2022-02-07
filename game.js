5cd3ed68faf36ffa3034c117e3dad51bd9d26775
Zeta-Text <holdenwllms@outlook.com>
2022-02-06 23:46:00 -0700

    Completed UI.

--- game.js
+++ game.js
@@ -19,57 +19,94 @@ let computerScore = 0;
 let score = [playerScore, computerScore];
 
 
+var playerSelection = "";
+function change2Rock() {
+    let local = "rock"
+    playerSelection = local;
+    
+}
+
+function change2Paper() {
+    let local = "paper"
+    playerSelection = local;
+    
+}
+
+function change2Scissors() {
+    let local = "scissors"
+    playerSelection = local;
+    
+}
+
 function playRound() {
 //change the return values to something more useful and console.log after each round
 // remember that console.log calls a function! 
-let playerSelection = prompt("What's your choice?");
+
 let computerSelection = computerPlay(choices);
+
+
   
     if (computerSelection == "rock" && playerSelection == "rock"){
-    
-        message = "Rock vs Rock. It's a tie!";
-        console.log(message); 
-        return message;
+        document.getElementById("resultMessage").innerHTML = "Rock vs Rock. It's a tie!";
+        document.getElementById("score").innerHTML = "Player score is: " + playerScore;
+        document.getElementById("compscore").innerHTML = "Computer score is: " + computerScore;
+        is3()
+        return;
     } else if (computerSelection == "rock" && playerSelection == "paper") {
         playerScore++;
-        message = "Rock vs Paper. You win!"
-        console.log(message); 
-        return message;
+        document.getElementById("resultMessage").innerHTML = "Rock vs Paper. You win!";
+        document.getElementById("score").innerHTML = "Player score is: " + playerScore;
+        document.getElementById("compscore").innerHTML = "Computer score is: " + computerScore; 
+        is3()
+        return;
     } else if (computerSelection == "rock" && playerSelection == "scissors") {
         computerScore++;
-        message = "Rock vs Scissors. You lose!"
-        console.log(message);
-        return message;   
+        document.getElementById("resultMessage").innerHTML = "Rock vs Scissors. You lose!";
+        document.getElementById("score").innerHTML = "Player score is: " + playerScore;
+        document.getElementById("compscore").innerHTML = "Computer score is: " + computerScore;
+        is3()
+        return;   
     } else if (computerSelection == "paper" && playerSelection == "paper") {
-        
-        message = "Paper vs Paper. It's a tie!"
-        console.log(message);
-        return message; 
+        document.getElementById("resultMessage").innerHTML = "Paper vs Paper. It's a tie!";
+        document.getElementById("score").innerHTML = "Player score is: " + playerScore;
+        document.getElementById("compscore").innerHTML = "Computer score is: " + computerScore;
+        is3()
+        return; 
     } else if (computerSelection == "paper" && playerSelection == "rock") {
         computerScore++;
-        message = "Paper vs Rock. You lose!"
-        console.log(message); 
-        return message; 
+        document.getElementById("resultMessage").innerHTML = "Paper vs Rock. You lose!";
+        document.getElementById("score").innerHTML = "Player score is: " + playerScore;
+        document.getElementById("compscore").innerHTML = "Computer score is: " + computerScore; 
+        is3()
+        return; 
     } else if (computerSelection == "paper" && playerSelection == "scissors") {
         playerScore++;
-        message = "Paper vs Scissors. You win!"
-        console.log(message); 
-        return message 
+        document.getElementById("resultMessage").innerHTML = "Paper vs Scissors. You win!";
+        document.getElementById("score").innerHTML = "Player score is: " + playerScore;
+        document.getElementById("compscore").innerHTML = "Computer score is: " + computerScore; 
+        is3()
+        return;
     } else if (computerSelection == "scissors" && playerSelection == "scissors") {
-        
-        message = "Scissors vs Scissors. It's a tie!"
-        console.log(message); 
-        return message 
+        document.getElementById("resultMessage").innerHTML = "Scissors vs Scissors. It's a tie!";
+        document.getElementById("score").innerHTML = "Player score is: " + playerScore;
+        document.getElementById("compscore").innerHTML = "Computer score is: " + computerScore; 
+        is3()
+        return; 
     } else if (computerSelection == "scissors" && playerSelection == "paper") {
         computerScore++;
-        message = "Scissors vs Paper. You lose!" 
-        console.log(message); 
-        return message
+        document.getElementById("resultMessage").innerHTML = "Scissors vs Paper. You lose!" ;
+        document.getElementById("score").innerHTML = "Player score is: " + playerScore;
+        document.getElementById("compscore").innerHTML = "Computer score is: " + computerScore; 
+        is3()
+        return;
     } else if (computerSelection == "scissors" && playerSelection == "rock") {
         playerScore++;
-        message = "Scissors vs Rock. You win!"
-        console.log(message); 
-        return message;
+        document.getElementById("resultMessage").innerHTML = "Scissors vs Rock. You win!" ;
+        document.getElementById("score").innerHTML = "Player score is: " + playerScore;
+        document.getElementById("compscore").innerHTML = "Computer score is: " + computerScore; 
+        is3()
+        return;
+
         
     } else {
         message = "something went wrong";
@@ -79,13 +116,57 @@ let computerSelection = computerPlay(choices);
 
 }
 
-function game() {
-   while (playerScore || computerScore < 3) {
-        playRound()
-        console.log("Computer: " + computerScore, "Player: " + playerScore);
+function is3() {
+    if (playerScore === 3) {
+        document.getElementById("winningmsg").innerHTML = "Player wins 3 rounds!";
+        return
+    } else if (computerScore === 3) {
+        document.getElementById("winningmsg").innerHTML = "Computer wins 3 rounds!";
+        return
+    } else {
+        return
     }
+
 }
-game()
-console.log("Computer: " + computerScore, "Player: " + playerScore);
+
+ 
+function game() {
+    for (let i = 0; i < 15; i++) {
+        if (playerScore === 3) {
+            return;
+            
+        } else if (computerScore === 3) {
+            return
+            
+        } else {
+            playRound();
+            return;
+            
+        }
+    }
+ }
+ 
+
+
+rock.addEventListener('click',() => {    
+    change2Rock();
+    game();    
+
+});
+
+
+paper.addEventListener('click',() => {    
+    change2Paper();
+    game();    
+
+});
+scissors.addEventListener('click',() => {    
+    change2Scissors();
+    game();    
+
+});
+
+
+
 
 
